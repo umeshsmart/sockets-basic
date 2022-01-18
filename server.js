e3a847f5ac0a5dc9cd9881b204f71107273cb6f0
umeshsmart <umesh.jangid@smartsensesolutions.com>
2022-01-18 21:39:35 +0530

    add leave

--- server.js
+++ server.js
@@ -17,6 +17,25 @@ io.on('connection',function(socket)
 {
 	console.log('User connected via socket.io !!!!');
 
+	socket.on('disconnect',function()
+	{
+		if(typeof clientInfo[socket.id] !== 'undefined')
+		{
+			var userData=clientInfo[socket.id];
+			socket.leave(userData.room);
+			socket.broadcast.to(userData.room).emit('message',
+			{
+				name:'System',
+				text:userData.name+' has leave room '+ userData.room+'!!',
+				timestamp:moment().valueOf()
+			});
+
+			delete clientInfo[socket.id];
+
+		}
+
+	});
+
 	socket.on('joinRoom',function(req,res)
 	{
 		clientInfo[socket.id]=req;
