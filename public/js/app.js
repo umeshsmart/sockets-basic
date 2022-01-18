8e176e5598b4cd1f1b9df7e27011306e9cf902ef
umeshsmart <umesh.jangid@smartsensesolutions.com>
2022-01-18 20:55:50 +0530

    Add name

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,11 @@
 //initialize
 var socket=io();
 
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name + ' wants to join ' + room);
+
 socket.on('connect',function()
 {
 	console.log('connected to socket.io server !!');
@@ -11,7 +16,9 @@ socket.on('message',function(message)
 	var momentTimestamp=moment.utc(message.timestamp);
 	console.log("New message :");
 	console.log(message.text);
-	jQuery('.messages').append('<p> <strong> '+momentTimestamp.local().format('h:mm a') +' </strong>'+ message.text +'</p>');
+	var $message=jQuery('.messages');
+	$message.append('<p> <strong> '+ message.name +' '+ momentTimestamp.local().format('h:mm a') +' </strong></p>');
+	$message.append('<p>'+ message.text +'</p>');
 });
 
 //sending the message data from the form
@@ -23,6 +30,7 @@ $form.on('submit',function(event)
 
 	var $message = $form.find('input[name=message]'); 
 	socket.emit('message',{
+		name:name,
 		text: $message.val()
 	});
 
