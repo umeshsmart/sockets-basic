c823166549a010046ad10b5e9d6e50598a289725
umeshsmart <umesh.jangid@smartsensesolutions.com>
2022-01-18 18:02:39 +0530

    Add front-end form to send message

--- public/js/app.js
+++ public/js/app.js
@@ -11,3 +11,19 @@ socket.on('message',function(message)
 	console.log("New message :");
 	console.log(message.text);
 });
+
+//sending the message data from the form
+var $form = jQuery('#message-form');
+
+$form.on('submit',function(event)
+{
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]'); 
+	socket.emit('message',{
+		text: $message.val()
+	});
+
+	$message.val('');
+	
+});
