1a17e43098578a5f6e148c2922a8c15d8afa86ba
umeshsmart <umesh.jangid@smartsensesolutions.com>
2022-01-18 17:24:11 +0530

    Add socket.io client and server

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ var PORT =process.env.PORT || 3000;
 //get express
 var express = require('express');
 var app=express();
-
 //set http server
 var http=require('http').Server(app);
+//socket
+var io=require('socket.io')(http);
 
 //use the folder as server
 app.use(express.static(__dirname + '/public'));
+
+//on listen event
+io.on('connection',function()
+{
+	console.log('User connection established via socket.io !!!!');
+});
 
 http.listen(PORT,function()
 {
